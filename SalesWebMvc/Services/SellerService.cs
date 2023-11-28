efda4febc2c8f331638599f7c09ce214d651c2dc
alexsandev <alex.rsjr10@gmail.com>
2023-11-28 14:05:45 -0300

    Delete Seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -13,16 +13,28 @@ namespace SalesWebMvc.Services
             _context = context;
         }
 
-        public List<Seller> FindAll()
-        {
-            return _context.Seller.ToList();
-        }
-
         public void Insert(Seller seller)
         {
             seller.Department = _context.Department.Where(d => d.Id == seller.Department.Id).First();
             _context.Add(seller);
             _context.SaveChanges();
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+
+        public Seller? FindById(int id) {
+            return _context.Seller.FirstOrDefault(s => s.Id == id);
+        }
+
+        public void Delete(int id) {
+            var obj = FindById(id);
+            if(obj != null){
+                _context.Seller.Remove(obj);
+                _context.SaveChanges();
+            } 
         }
     }
 }
